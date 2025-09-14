c98543ff906076f05faa071d452f7c5daa9ca96d
Hadi Faraz <hadifaraz52@proton.me>
2025-09-14 12:05:36 -0600

    Add host configuration for my server

---
+++ hosts/server/default.nix
@@ -0,0 +1,76 @@
+{ config, pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    ../common.nix
+  ];
+
+  networking.hostName = "server";
+
+  services = {
+    cockpit = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+
+    };
+
+    httpd = {
+      enable = true;
+      adminAddr = "hadifaraz52@protonmail.com";
+      virtualHosts = {
+        "cockpit.server.bepistypebeat.com" = {
+          forceSSL = true;
+          sslServerCert = "/var/lib/acme/cockpit.server.bepistypebeat.com/fullchain.pem";
+          sslServerKey = "/var/lib/acme/cockpit.server.bepistypebeat.com/key.pem";
+          extraConfig = ''
+            SSLProxyEngine On
+            ProxyRequests Off
+
+            ProxyPass / https://127.0.0.1:9090/
+            ProxyPassReverse / https://127.0.0.1:9090/
+
+
+            ProxyPass "/socket" "wss://127.0.0.1:9090/socket"
+            ProxyPassReverse "/socket" "wss://127.0.0.1:9090/socket"
+            RequestHeader set Origin "https://cockpit.server.bepistypebeat.com"
+          '';
+        };
+      };
+      extraModules = [
+        "proxy"
+        "proxy_http"
+        "proxy_wstunnel"
+        "ssl"
+      ];
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "hadifaraz52@protonmail.com";
+    certs = {
+      "cockpit.server.bepistypebeat.com" = {
+        dnsProvider = "cloudflare";
+        credentialsFile = "/etc/nixos/secrets/cloudflare-creds";
+      };
+    };
+  };
+
+  systemd = {
+    extraConfig = ''
+      [Login]
+      HandleLidSwitch=ignore
+      HandleLidSwitchDocked=ignore
+      HandleLidSwitchExternalPower=ignore
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    cockpit
+    apacheHttpd
+  ];
+}
