034b8156ee5cfae409b8a3a9eb0a68497791695e
Hadi Faraz <hadifaraz52@proton.me>
2025-09-14 11:20:49 -0600

    Sort imports alphabetically in programs

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }: {
   imports = [
     ./browsers.nix # Any browsers I want and their config
-    ./git.nix # Git related packages
-    ./media.nix # All media related packages
-    ./xdg.nix # XDG Portal setup
-    ./re.nix # All Reverse Engineering related programs (rizin, cutter, frida, etc.)
-    ./dev.nix # All programming related programs (vscode, distrobox, devcontainer, toolbox)
-    ./office.nix # All file editing programs (libreoffice, wpsoffice, obsidian)
-    ./im.nix # All instant messaging programs (discord, telegram)
+    ./cli.nix
     ./design.nix # All media creation related programs (blender, CAD tools, etc.)
+    ./dev.nix # All programming related programs (vscode, distrobox, devcontainer, toolbox)
     ./gaming.nix # Gaming related programs
+    ./git.nix # Git related packages
+    ./im.nix # All instant messaging programs (discord, telegram)
+    ./media.nix # All media related packages
+    ./office.nix # All file editing programs (libreoffice, wpsoffice, obsidian)
+    ./re.nix # All Reverse Engineering related programs (rizin, cutter, frida, etc.)
+    ./xdg.nix # XDG Portal setup
   ];
 }
