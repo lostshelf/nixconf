c766a8cdcd27c2317903046a378e770308d60fa2
Hadi Faraz <hadifaraz52@protonmail.com>
2025-09-30 13:15:39 -0600

    Add JetBrain's IDEs

--- home/programs/dev.nix
+++ home/programs/dev.nix
@@ -1,4 +1,23 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
+let
+  jetbrain_ides = with pkgs.jetbrains; [
+    clion
+    goland
+    idea-ultimate
+    rust-rover
+    webstorm
+    phpstorm
+    writerside
+    rider
+    ruby-mine
+    pycharm-professional
+    mps
+    gateway
+    dataspell
+    datagrip
+    aqua
+  ];
+in {
   home.packages = with pkgs; [
     docker
     kubectl
@@ -22,7 +41,9 @@
     cmake
     ninja
     gnum4
-  ];
+    android-studio-full
+    android-studio-tools
+  ] ++ jetbrain_ides;
 
   programs = {
     tmux = {
