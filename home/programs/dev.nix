347b6c392e58b740d707639dc13376df6bec87a8
Hadi Faraz <hadifaraz52@protonmail.com>
2025-09-30 13:19:24 -0600

    Refactor the let declaration

--- home/programs/dev.nix
+++ home/programs/dev.nix
@@ -1,49 +1,50 @@
-{ lib, pkgs, ... }:
-let
-  jetbrain_ides = with pkgs.jetbrains; [
-    clion
-    goland
-    idea-ultimate
-    rust-rover
-    webstorm
-    phpstorm
-    writerside
-    rider
-    ruby-mine
-    pycharm-professional
-    mps
-    gateway
-    dataspell
-    datagrip
-    aqua
-  ];
-in {
-  home.packages = with pkgs; [
-    docker
-    kubectl
-    distrobox
-    podman
-    devcontainer
-    jetbrains-toolbox
-    python3 # Needed for IDA
-    qemu
-    rustup
-    ghc
-    cabal-install
-    nim
-    clisp
-    nasm
-    go
-    (lib.hiPrio gcc)
-    (lib.lowPrio clang)
-    toybox
-    gnumake
-    cmake
-    ninja
-    gnum4
-    android-studio-full
-    android-studio-tools
-  ] ++ jetbrain_ides;
+{ lib, pkgs, ... }: {
+  home.packages = let
+    jetbrain_ides = with pkgs.jetbrains; [
+      clion
+      goland
+      idea-ultimate
+      rust-rover
+      webstorm
+      phpstorm
+      writerside
+      rider
+      ruby-mine
+      pycharm-professional
+      mps
+      gateway
+      dataspell
+      datagrip
+      aqua
+    ];
+    dev_pkgs = with pkgs; [
+      docker
+      kubectl
+      distrobox
+      podman
+      devcontainer
+      jetbrains-toolbox
+      python3 # Needed for IDA
+      qemu
+      rustup
+      ghc
+      cabal-install
+      nim
+      clisp
+      nasm
+      go
+      (lib.hiPrio gcc)
+      (lib.lowPrio clang)
+      toybox
+      gnumake
+      cmake
+      ninja
+      gnum4
+      android-studio-full
+      android-studio-tools
+    ]
+  in
+    dev_pkgs++ jetbrain_ides;
 
   programs = {
     tmux = {
