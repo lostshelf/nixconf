a191e397e0e3dc74723c51e41cfd31b1bab6f942
baka <hadifaraz52@protonmail.com>
2025-10-26 01:10:52 -0600

    Change pkgs to npkgs to not cause variable shadowing

--- home/programs/gaming.nix
+++ home/programs/gaming.nix
@@ -1,15 +1,16 @@
 { libs, pkgs, unstable, ... }: {
-  home.packages = let
-    pkgs = with pkgs; [
-      prismlauncher
-    ];
-    upkgs = with unstable; [
-      umu-launcher
-      lutris
-      protonup-qt
-      gamescope
-      r2modman
-    ];
-  in
-    pkgs ++ upkgs;
+    home.packages = let
+      npkgs = with pkgs; [
+        prismlauncher
+      ];
+      upkgs = with unstable; [
+        umu-launcher
+        lutris
+        protonup-qt
+        gamescope
+        r2modman
+      ];
+    in
+      npkgs ++ upkgs;
+
 }
