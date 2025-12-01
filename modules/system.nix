dec0aa389b6d64bb6f546e1b2cbe0c4e5a5d281c
baka <hadifaraz52@protonmail.com>
2025-11-30 19:39:14 -0700

    Add extra packages to steam

--- modules/system.nix
+++ modules/system.nix
@@ -121,6 +121,16 @@
         extraPkgs = pkgs: with pkgs; [
           gamescope
           gamemode
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
         ];
       };
     };
