66336aa93d4bb16a2f6831c86eba140cb464e257
baka <hadifaraz52@protonmail.com>
2025-11-30 19:23:31 -0700

    Replace noto-fonts-emoji with noto-fonts-color-emoji

--- modules/common.nix
+++ modules/common.nix
@@ -108,7 +108,7 @@
 
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
 
       nerd-fonts.symbols-only # symbols icon only
       nerd-fonts.fira-code
