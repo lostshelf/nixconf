e38d1421e5d1edbd93f6a6d4209fd2803333ee59
Hadi Faraz <hadifaraz52@protonmail.com>
2026-07-10 14:17:05 -0600

    Install Nix Language Server

--- modules/features/dev.nix
+++ modules/features/dev.nix
@@ -1,5 +1,9 @@
 { self, inputs, ... }: {
   flake.nixosModules.devConfig = { pkgs, lib, config, ... }: {
+    environment.systemPackages = with pkgs; [
+      nil
+    ];
+
     nixpkgs.config.android_sdk.accept_license = true;
   };
 }
