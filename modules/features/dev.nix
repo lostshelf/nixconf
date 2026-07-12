09e706461f7f0a0e7543ed84abb093bf982deb02
Hadi Faraz <hadifaraz52@protonmail.com>
2026-07-11 21:55:02 -0600

    Install Godot

--- modules/features/dev.nix
+++ modules/features/dev.nix
@@ -2,6 +2,8 @@
   flake.nixosModules.devConfig = { pkgs, lib, config, ... }: {
     environment.systemPackages = with pkgs; [
       nil
+
+      godot
     ];
 
     nixpkgs.config.android_sdk.accept_license = true;
