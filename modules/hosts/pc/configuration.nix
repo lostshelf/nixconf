ef41e4c0578b4ad0f42bb5da950fca1ffbc2254a
Hadi Faraz <hadifaraz52@protonmail.com>
2026-07-31 22:09:13 -0600

    Disable low latency pipewire config that was causing audio crackling

--- modules/hosts/pc/configuration.nix
+++ modules/hosts/pc/configuration.nix
@@ -177,14 +177,15 @@
           ];
         };
 
-        "92-low-latency" = {
-          "context.properties" = {
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 32;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 32;
-          };
-        };
+        # This was causing some audio crackling
+        # "92-low-latency" = {
+        #   "context.properties" = {
+        #     "default.clock.rate" = 48000;
+        #     "default.clock.quantum" = 32;
+        #     "default.clock.min-quantum" = 32;
+        #     "default.clock.max-quantum" = 32;
+        #   };
+        # };
       };
     };
 
@@ -195,7 +196,7 @@
       after = [ "graphical-session.target" "pipewire.service" ];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w \"DP-2;width=1920;height=1080;halign=center;valign=center\" -c mkv -s 1920x1080 -f 60 -cursor yes -a \"alsa_input.usb-HP__Inc_HyperX_Cloud_III_Wireless_0000000000000000-00.mono-fallback|alsa_output.usb-HP__Inc_HyperX_Cloud_III_Wireless_0000000000000000-00.analog-stereo.monitor\" -k hevc_10bit -bm cbr -q 30000 -cr full -tune quality -r 300 -replay-storage ram -df yes -restart-replay-on-save yes -o /home/hadif/Videos/Clips";
+        ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w \"DP-2;width=1920;height=1080;halign=center;valign=center\" -c mkv -s 1920x1080 -f 60 -cursor yes -a \"alsa_input.usb-HP__Inc_HyperX_Cloud_III_Wireless_0000000000000000-00.mono-fallback|alsa_output.usb-HP__Inc_HyperX_Cloud_III_Wireless_0000000000000000-00.analog-stereo.monitor\" -k hevc_10bit -bm cbr -q 30000 -cr full -tune quality -r 300 -replay-storage ram -df yes -restart-replay-on-save no -o /home/hadif/Videos/Clips";
       };
     };
 
