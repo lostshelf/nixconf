de9bcf9177148d335ffafce25fb220faff00a849
Hadi Faraz <hadifaraz52@protonmail.com>
2026-08-04 13:00:11 -0600

    Set firefox as default browser at home-manager level

--- modules/home/misc.nix
+++ modules/home/misc.nix
@@ -1,5 +1,16 @@
 { self, inputs, ... }: {
   flake.homeModules.miscConfig = { pkgs, ... }: {
     services.mpris-proxy.enable = true;
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+      };
+    };
   };
 }
