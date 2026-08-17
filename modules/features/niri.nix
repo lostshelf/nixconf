5246f8281152476d7a103c65a52aaec9115ebc47
Hadi Faraz <hadifaraz52@protonmail.com>
2026-08-17 12:41:39 -0600

    niri: add noctalia shell wrapper definition

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -27,5 +27,12 @@
         };
       };
     };
+
+    packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+      inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
+      settings =
+        (builtins.fromJSON
+          (builtins.readFile ./noctalia.json)).settings;
+    };
   };
 }
