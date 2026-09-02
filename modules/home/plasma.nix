3ab229ff72473c033f39be2d26aab6b551b492ca
Hadi Faraz <hadifaraz52@protonmail.com>
2026-09-01 21:31:18 -0600

    apps: fix application entry for brave

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -96,7 +96,7 @@
             {
               iconTasks = {
                 launchers = [
-                  "applications:com.brave.Browser.desktop"
+                  "applications:brave-browser.desktop"
                   "applications:discord-canary.desktop"
                   "applications:vesktop.desktop"
                   "applications:org.kde.dolphin.desktop"
